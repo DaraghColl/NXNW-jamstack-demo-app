7366dea77cf23ead5d59ce262e2db70563045031
DaraghColl <daraghcoll@hotmail.com>
2020-09-25 23:26:49 +0100

    add try catch to checkout function

--- functions/checkout.js
+++ functions/checkout.js
@@ -4,15 +4,27 @@ const stripe = require('stripe')(stripePrivateKey);
 exports.handler = async (event, context) => {
   const data = JSON.parse(event.body);
 
-  const charge = await stripe.charges.create({
-    currency: 'eur',
-    source: 'tok_us',
-    amount: data.amount,
-    description: 'Payment from jamstack website',
-  });
+  try {
+    const charge = await stripe.charges.create({
+      currency: 'eur',
+      source: 'tok_us',
+      amount: data.amount,
+      description: 'Payment from jamstack website',
+    });
 
-  return {
-    statusCode: 200,
-    body: 'hello',
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        msg: "Payment Success"
+      }),
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        msg: "Payment Failure"
+      }),
+    };
+  }
 };
