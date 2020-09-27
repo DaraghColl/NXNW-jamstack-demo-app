339a0b49520033194c5beaa39252b3d1ddf89021
DaraghColl <daraghcoll@hotmail.com>
2020-09-27 03:24:47 +0100

    form styles, submit icon and animation

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -31,7 +31,14 @@
           required
           v-model="formData.email"
         />
-        <label for="contact_email" class="form__label">Email</label>
+        <label
+          for="contact_email"
+          class="form__label"
+          v-bind:class="{
+            'label-floated': formData.email !== '',
+          }"
+          >Email</label
+        >
       </div>
 
       <!-- message -->
@@ -46,7 +53,22 @@
         <label for="contact_message" class="form__label">Message</label>
       </div>
       <div class="submit-form">
-        <button class="primary-button" type="submit">Send</button>
+        <button class="primary-button" type="submit">
+          Send
+          <svg
+            class="submit-icon"
+            v-bind:class="{ 'email-bounce': emailSending }"
+            fill="none"
+            height="24"
+            width="24"
+          >
+            <path
+              d="M2 6C2 4.89543 2.89543 4 4 4H20C21.1046 4 22 4.89543 22 6V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18V6ZM5.51859 6L12 11.6712L18.4814 6H5.51859ZM20 7.32877L12.6585 13.7526C12.2815 14.0825 11.7185 14.0825 11.3415 13.7526L4 7.32877V18H20V7.32877Z"
+              fill="#fff"
+            ></path>
+          </svg>
+          <!-- <g-image class="send mail" src="/icons/email.svg" alt="send mail" /> -->
+        </button>
       </div>
     </form>
   </section>
@@ -57,7 +79,7 @@ import emailjs from 'emailjs-com';
 
 export default {
   data: () => ({
-    showEmailMessage: false,
+    emailSending: false,
     formData: {
       name: '',
       email: '',
@@ -70,22 +92,27 @@ export default {
       const user_id = process.env.GRIDSOME_EMAIL_JS_USER_ID;
       const template_id = process.env.GRIDSOME_EMAIL_JS__TEMPLATE_ID;
 
+      this.emailSending = true;
+
       emailjs.sendForm(service_id, template_id, e.target, user_id).then(
         result => {
-          this.showEmailMessage = true;
           this.messageType = 'success';
-          let form = document.getElementById('contact-form');
           this.$toasted.show('Email Sent!');
-          form.reset();
+          this.resetForm();
+          this.emailSending = false;
         },
         error => {
-          this.showEmailMessage = true;
           this.messageType = 'error';
-          let form = document.getElementById('contact-form');
           this.$toasted.show('Error Sending Email');
-          form.reset();
+          this.resetForm();
+          this.emailSending = false;
         }
       );
+    },
+    resetForm() {
+      this.formData.name = '';
+      this.formData.email = '';
+      this.formData.message = '';
     },
   },
 };
@@ -102,6 +129,7 @@ export default {
     text-align: center;
   }
 
+  /* input wrapper */
   .float {
     width: 100%;
     margin-left: auto;
@@ -112,6 +140,7 @@ export default {
     margin-bottom: 1.25rem;
     position: relative;
 
+    /* input */
     .form__item {
       background-color: transparent;
       border: none;
@@ -120,7 +149,6 @@ export default {
       outline: none;
       height: 1em;
       width: 100%;
-      font-size: 16px;
       margin: 0 0 8px 0;
       padding: 0 0 0.5em 0;
 
@@ -129,6 +157,7 @@ export default {
         transition: 0.8s;
         outline: none;
       }
+
       &:valid,
       &:focus {
         & ~ .form__label {
@@ -147,20 +176,66 @@ export default {
       position: absolute;
       top: 20px;
       left: 0;
-      font-size: 1rem;
+      font-size: 1em;
       transition: 0.2s;
-
       cursor: text;
     }
 
     .form__message {
-      height: 2em;
+      height: 1.5em;
+
+      &:valid,
+      &:focus {
+        & ~ .form__label {
+          top: -5px;
+        }
+      }
     }
   }
 
+  /* submit button */
   .submit-form {
     display: flex;
     justify-content: flex-end;
+    transition: 0.3s;
+
+    button {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+
+      &:hover {
+        .submit-icon {
+          path {
+            fill: $primary;
+            transition: 0.3s;
+          }
+        }
+      }
+    }
+  }
+}
+
+/* floating class for email invalid & not focused  */
+.label-floated {
+  top: 0 !important;
+  font-size: 0.8em !important;
+  color: $primary !important;
+  transition: 0.2s;
+}
+
+.email-bounce {
+  animation: email-bounce 0.5s ease infinite;
+}
+@keyframes email-bounce {
+  100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-10px);
+  }
+  60% {
+    transform: translateY(-5px);
   }
 }
 </style>
